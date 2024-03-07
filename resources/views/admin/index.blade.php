007d4ff1282af6b757ddfd37ce7f4c63193eaab2
Tanvir Ahmed Raihan <raihan.tanvir961@gmail.com>
2024-03-07 16:49:36 +0600

    "admin mastering"

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,11 @@
-<x-app-layout>
-   
-</x-app-layout>
+<!DOCTYPE html>
+<html>
+  <head> 
+    @include('admin.css')
+  </head>
+  <body>
+    @include('admin.header')
+    @include('admin.SidebarNavigation')
+    @include('admin.javascript')
+  </body>
+</html>
